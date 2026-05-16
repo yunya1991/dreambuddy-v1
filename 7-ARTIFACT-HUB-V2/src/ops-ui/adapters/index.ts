c0f6980a8ff8bd9e345909146d129cbdaca28542
yunya1991 <32544013+yunya1991@users.noreply.github.com>
2026-05-17 03:21:19 +0800

    feat(ops-ui): add adapters barrel export

---
+++ 7-ARTIFACT-HUB-V2/src/ops-ui/adapters/index.ts
@@ -0,0 +1,3 @@
+export { HealthContractV1, HealthViewModel, toHealthViewModel } from "./healthAdapter";
+export { TraceContractV1, TraceViewModel, toTraceViewModel } from "./traceAdapter";
+export { WorkflowContractV1, WorkflowViewModel, WorkflowChainPhase as ChainPhase, toWorkflowViewModel } from "./workflowAdapter";
